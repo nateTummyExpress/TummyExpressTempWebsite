79632e1e18d632453648018febe8e385b5c3ab39
ngreenetechnic <ngreene@technicpos.com>
2024-08-10 08:06:53 -0400

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+(function ($) {
+  'use strict';
+
+  $(window).on('load', function () {
+    $('#preloader').fadeOut('slow', function () {
+      $(this).remove();
+    });
+  });
+ 
+
+})(jQuery);
