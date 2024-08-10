1c4ff639e28692217bb8c1871c250172c2edc158
ngreenetechnic <ngreene@technicpos.com>
2024-08-10 18:27:34 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,3 +14,11 @@
 $('#signButton').click(function(){
   $('#hiddenForm').show();
 });
+
+/* Google form mobile */
+@media screen and (max-width:767px) {
+iframe[src*="google.com/forms"] {
+    width: 100% !important;
+    height: 800px;
+}
+}
